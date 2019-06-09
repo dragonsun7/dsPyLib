703e1ae87d02fbc930fecd53e4aa48b34eb6433c
Dragon Sun <dragonsun7@163.com>
2019-06-10 00:52:24 +0800

    * utils/datetime.py
    utils/datetime.py 添加：timestamp_to_iso, iso_to_datetime, datetime_to_iso
    版本号提高到0.0.14

--- dsPyLib/utils/datetime.py
+++ dsPyLib/utils/datetime.py
@@ -1,12 +1,11 @@
 # -*-coding:utf-8-*-
 __author__ = 'Dragon Sun'
 
-
 import datetime
 import calendar
 import math
 from enum import Enum
-
+import dateutil.parser as date_parser
 
 """
 "%Y-%m-%d %H:%M:%S.%f"
@@ -138,3 +137,38 @@ def next_time(dt, interval, unit):
     if unit == CycleUnit.year:
         dt = month_delta(dt, interval * 12)
     return dt
+
+
+def timestamp_to_iso(ts):
+    """
+    将timestamp转换为ISO8601字符串
+    例如：
+        timestamp_to_iso(time.time()) => 2019-04-07T12:51:30.000Z
+    :param ts: timestamp
+    :return: str
+    """
+    date = datetime.datetime.utcfromtimestamp(ts)
+    iso = date.isoformat('T', 'milliseconds') + 'Z'
+    return iso
+
+
+def iso_to_datetime(s: str) -> datetime.datetime:
+    """
+    将ISO8601字符串转换为datetime.datetime
+    :param s: str, ISO8601字符串，例如：'2019-04-13T12:50:00.000Z'
+    :return: datetime
+    """
+    return date_parser.parse(s)
+
+
+def datetime_to_iso(d: datetime.datetime) -> str:
+    """
+    将datetime转换为ISO8601字符串
+    :param d: datetime.datetime
+    :return: str, ISO8601字符串，例如：'2019-04-13T12:50:00.000Z'
+    """
+    if d:
+        d1 = datetime.datetime.utcfromtimestamp(d.timestamp())
+        return d1.isoformat(timespec='milliseconds') + 'Z'
+    else:
+        return ''
