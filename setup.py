c185248473577fa2fe91945e33e7ae1f19d97694
Dragon Sun <dragonsun7@163.com>
2020-01-31 15:33:16 +0800

    * 0.0.25 修复sound模块资源文件未能打包的问题

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ with open("README.md", "r") as fh:
 setuptools.setup(
     name="dsPyLib",
     keywords="dsPyLib",
-    version="0.0.24",
+    version="0.0.25",
     author="Dragon Sun",
     author_email="dragonsun7@163.com",
     license="MIT",
