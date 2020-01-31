c21d25075b3ec3c0fc2d2751f1760018a369b3e3
Dragon Sun <dragonsun7@163.com>
2020-01-31 11:05:03 +0800

    * 0.0.24 添加sound模块

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ with open("README.md", "r") as fh:
 setuptools.setup(
     name="dsPyLib",
     keywords="dsPyLib",
-    version="0.0.23",
+    version="0.0.24",
     author="Dragon Sun",
     author_email="dragonsun7@163.com",
     license="MIT",
@@ -29,5 +29,6 @@ setuptools.setup(
         "psycopg2-binary",
         "python-dateutil",
         "pandas",
+        "pyaudio",
     ]
 )
