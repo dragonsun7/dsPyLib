07805963b94b5973303d6da6f5dab7e681f9eb71
Dragon Sun <dragonsun7@163.com>
2020-01-31 15:39:16 +0800

    * 0.0.26 添加阿里云语音转换模块

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ with open("README.md", "r") as fh:
 setuptools.setup(
     name="dsPyLib",
     keywords="dsPyLib",
-    version="0.0.25",
+    version="0.0.26",
     author="Dragon Sun",
     author_email="dragonsun7@163.com",
     license="MIT",
