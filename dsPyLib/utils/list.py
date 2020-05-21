46de638a469774571f4b963c51522319d9bdd012
dragonsun7 <dragonsun7@163.com>
2020-05-21 10:16:30 +0800

    0.0.29

--- dsPyLib/utils/list.py
+++ dsPyLib/utils/list.py
@@ -5,9 +5,6 @@ __date__ = '2020-05-20 14:30:37'
 
 # 去掉列表中的重复项(http://www.chenxm.cc/article/541.html)
 def remove_duplicate(the_list: list):
-    # noinspection PyUnresolvedReferences
-    import datetime
-
     temp_list = list(set([str(j) for j in the_list]))
     the_list = [eval(j) for j in temp_list]
     return the_list
