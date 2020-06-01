22e60c03f6619e7684ccabd00a111e298a12f3a7
dragonsun7 <dragonsun7@163.com>
2020-06-02 00:39:37 +0800

    Create boolean.py

---
+++ dsPyLib/utils/boolean.py
@@ -0,0 +1,10 @@
+# -*- coding:utf-8 -*-
+__author__ = 'Dragon Sun'
+__date__ = '2020-06-02 00:07:45'
+
+
+def str_to_bool(s: str) -> bool:
+    if s.lower() == 'true':
+        return True
+    else:
+        return False
