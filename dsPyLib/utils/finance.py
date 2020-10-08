e7d66ba66eacf12d19a5a4618cd001105364be4d
dragonsun7 <dragonsun7@163.com>
2020-10-08 16:08:28 +0800

    Update finance.py

--- dsPyLib/utils/finance.py
+++ dsPyLib/utils/finance.py
@@ -3,8 +3,14 @@ __author__ = 'Dragon Sun'
 __date__ = '2020-10-08 12:08:58'
 
 
-# 将浮点数转换为保留指定小数位的字符串
+# 将浮点数转换为保留指定小数位的字符串(四舍五入)
 def to_str(f: float, digit: int = 2) -> str:
+    v = f * 10 ** (digit + 1)
+    d = v % 10
+    f = int(v / 10)
+    if d >= 5:
+        f += 1
+    f = f / 10 ** digit
     fmt = f'%.{digit}f'
     ret = fmt % f
     return ret
