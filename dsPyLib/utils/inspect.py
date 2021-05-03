3f7d4567ddf1b102bc6971bdbdcd9e11bab71e4d
dragonsun7 <dragonsun7@163.com>
2021-05-03 12:54:05 +0800

    Update inspect.py

--- dsPyLib/utils/inspect.py
+++ dsPyLib/utils/inspect.py
@@ -10,6 +10,30 @@ def get_current_function_name() -> str:
     return inspect.stack()[1][3]
 
 
+# 获取当前对象属性列表
+def get_attr_names(o, ignores: list[str] = None) -> list[str]:
+    """
+    获取对象自定义的属性列表
+        1. 排除掉'_'和'__'打头的
+        2. 排除掉方法
+        3. 排除掉自定义忽略列表中的属性
+    :return:
+    """
+
+    def is_keep(x: str) -> bool:
+        if x.startswith('_') or x.startswith('__'):
+            return False
+        if inspect.ismethod(getattr(o, x)):
+            return False
+        if ignores and (x in ignores):
+            return False
+        return True
+
+    attrs = o.__dir__()
+    ret = filter(is_keep, attrs)
+    return list(ret)
+
+
 def main():
     print(get_current_function_name())
 
