291d0d3055b6caf234e7c3043716fedba728b4b9
dragonsun7 <dragonsun7@163.com>
2022-05-16 13:39:42 +0800

    + peewee.db_base_model.DBStandardModel

--- dsPyLib/peewee/db_base_model.py
+++ dsPyLib/peewee/db_base_model.py
@@ -2,7 +2,10 @@
 __author__ = 'Dragon Sun'
 __date__ = '2020-05-18 01:15:37'
 
-from peewee import *
+import datetime
+
+from peewee import Model, DateTimeField, CharField
+
 from dsPyLib.peewee.db_mgr import DBMgr
 
 
@@ -12,3 +15,9 @@ class DBBaseModel(Model):
     class Meta:
         # database = database_proxy
         database = DBMgr.get_database()
+
+
+class DBStandardModel(DBBaseModel):
+    created = DateTimeField(default=datetime.datetime.now, formats='%Y-%m-%d %H:%M:%S.%f', help_text='创建时间')
+    updated = DateTimeField(default=datetime.datetime.now, formats='%Y-%m-%d %H:%M:%S.%f', help_text='更新时间')
+    remark = CharField(default='', max_length=512, help_text='备注')
