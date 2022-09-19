9d196150891056dfaae94c0615569db733a1357c
dragonsun7 <dragonsun7@163.com>
2022-09-19 12:18:44 +0800

    Update db_base_model.py

--- dsPyLib/peewee/db_base_model.py
+++ dsPyLib/peewee/db_base_model.py
@@ -2,9 +2,7 @@
 __author__ = 'Dragon Sun'
 __date__ = '2020-05-18 01:15:37'
 
-import datetime
-
-from peewee import Model, DateTimeField, CharField
+from peewee import Model, SQL, DateTimeField, CharField
 
 from dsPyLib.peewee.db_mgr import DBMgr
 
@@ -18,6 +16,8 @@ class DBBaseModel(Model):
 
 
 class DBStandardModel(DBBaseModel):
-    created = DateTimeField(default=datetime.datetime.now, formats='%Y-%m-%d %H:%M:%S.%f', help_text='创建时间')
-    updated = DateTimeField(default=datetime.datetime.now, formats='%Y-%m-%d %H:%M:%S.%f', help_text='更新时间')
+    created = DateTimeField(formats='%Y-%m-%d %H:%M:%S.%f', constraints=[SQL('DEFAULT CURRENT_TIMESTAMP')],
+                            help_text='创建时间')
+    updated = DateTimeField(formats='%Y-%m-%d %H:%M:%S.%f', constraints=[SQL('DEFAULT CURRENT_TIMESTAMP')],
+                            help_text='更新时间')
     remark = CharField(default='', max_length=512, help_text='备注')
