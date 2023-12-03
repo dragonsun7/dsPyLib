7eacce060705058ae2f5aa5a1c089df5a36ea03a
dragonsun7 <dragonsun7@163.com>
2023-12-03 18:54:06 +0800

    Update pandas_config.py

--- dsPyLib/pandas/pandas_config.py
+++ dsPyLib/pandas/pandas_config.py
@@ -4,6 +4,7 @@ __date__ = '2019-06-28 21:47:04'
 
 import pandas
 
+# 设置中文列头对齐：https://zhuanlan.zhihu.com/p/497233470
 # https://blog.csdn.net/weekdawn/article/details/81389865
 
 # 显示所有列
@@ -16,7 +17,9 @@ pandas.set_option('display.width', None)
 pandas.set_option('max_colwidth', 200)
 # 中文列头对齐
 pandas.set_option('display.unicode.ambiguous_as_wide', True)
-pandas.set_option('display.unicode.east_asian_width', True)
+pandas.set_option('display.unicode.east_asian_width', True)  # 输出右对齐
+# 列头居中对齐
+pandas.set_option('display.colheader_justify', 'center')
 
 
 # 为了防止IDE优化掉包引用
